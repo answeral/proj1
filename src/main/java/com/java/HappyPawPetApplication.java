19ff9ee478725aa6b1829aaaf53b94133a20184c
igyuwon <igyuwon60@gmail.com>
2024-08-04 16:21:46 +0900

    [REFACTOR] Rename Application

--- src/main/java/com/java/HappyPawPetApplication.java
+++ src/main/java/com/java/HappyPawPetApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainScreenApplication {
+public class HappyPawPetApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MainScreenApplication.class, args);
+		SpringApplication.run(HappyPawPetApplication.class, args);
 	}
 
 }
